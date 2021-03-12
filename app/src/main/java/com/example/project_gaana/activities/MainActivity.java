aa11921580aede18b4fea539849e4ab51962d9ff
soubiakulsum <soubiakulsum@gmail.com>
2021-03-13 02:00:43 +0530

    Merge branch 'main' of https://github.com/soubiakulsum/gaana into soubia
     Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/example/project_gaana/activities/MainActivity.java
    	app/src/main/java/com/example/project_gaana/fragments/AllSongsFragment.java
    	app/src/main/res/drawable/ghungroo.jpg
    	app/src/main/res/drawable/ghungrooo.jpg
    	app/src/main/res/drawable/ghungrooooo.jpg
    english songs fragment done
    didnt get api service
    a
    notification part pending
    
    changes in mainactivity and icons done

--- app/src/main/java/com/example/project_gaana/activities/MainActivity.java
+++ app/src/main/java/com/example/project_gaana/activities/MainActivity.java
@@ -1,18 +1,17 @@
 package com.example.project_gaana.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.project_gaana.R;
 import com.example.project_gaana.adapter.HomeAdapter;
@@ -20,7 +19,6 @@ import com.example.project_gaana.fragments.BuzzExtended;
 import com.example.project_gaana.fragments.BuzzFragment;
 import com.example.project_gaana.fragments.FragmentListener;
 import com.example.project_gaana.fragments.MusicFragment;
-import com.example.project_gaana.fragments.PodcastFragment;
 import com.google.android.material.tabs.TabLayout;
 
 
@@ -35,6 +33,10 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
     private ImageView Mnavi4;
     private ImageView Mnavi2;
     private ImageView Mnavi1;
+    private TextView naviText1;
+    private TextView naviText2;
+    private TextView naviText3;
+    private TextView naviText4;
     private ImageButton btnSearch;
 
     private FragmentManager fragmentManager;
@@ -65,6 +67,10 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
         Mnavi4 = findViewById(R.id.navi4);
         Mnavi2 = findViewById(R.id.navi2);
         Mnavi1 = findViewById(R.id.navi1);
+        naviText1 = findViewById(R.id.naviText1);
+        naviText2 = findViewById(R.id.naviText2);
+        naviText3 = findViewById(R.id.naviText3);
+        naviText4 = findViewById(R.id.naviText4);
         btnSearch = findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +86,14 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
         Mnavi1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                Mnavi1.setImageResource(R.drawable.ic_home_selected);
+//                Mnavi2.setImageResource(R.drawable.ic_hotshots__4_);
+//                Mnavi3.setImageResource(R.drawable.ic_flash_symbol);
+//                Mnavi4.setImageResource(R.drawable.ic_headphones);
+//                naviText1.setTextColor(Color.parseColor("000000"));
+//                naviText3.setTextColor(Color.parseColor("CACFD2"));
+//                naviText4.setTextColor(Color.parseColor("CACFD2"));
+//                naviText2.setTextColor(Color.parseColor("CACFD2"));
                 Intent intent = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(intent);
             }
@@ -87,12 +101,29 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
         Mnavi2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Mnavi1.setImageResource(R.drawable.ic_home);
+                Mnavi2.setImageResource(R.drawable.ic_hotshots_selected);
+                Mnavi3.setImageResource(R.drawable.ic_flash_symbol);
+                Mnavi4.setImageResource(R.drawable.ic_headphones);
+//                naviText1.setTextColor(Color.parseColor("CACFD2"));
+//                naviText2.setTextColor(Color.parseColor("000000"));
+//                naviText3.setTextColor(Color.parseColor("CACFD2"));
+//                naviText4.setTextColor(Color.parseColor("CACFD2"));
+
                 AddPodcast();
             }
         });
         Mnavi4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Mnavi1.setImageResource(R.drawable.ic_home);
+                Mnavi2.setImageResource(R.drawable.ic_hotshots__4_);
+                Mnavi3.setImageResource(R.drawable.ic_flash_symbol);
+                Mnavi4.setImageResource(R.drawable.ic_music_selected__1_);
+//                naviText1.setTextColor(Color.parseColor("CACFD2"));
+//                naviText2.setTextColor(Color.parseColor("CACFD2"));
+//                naviText3.setTextColor(Color.parseColor("000000"));
+//                naviText4.setTextColor(Color.parseColor("CACFD2"));
                 AddMusic();
             }
         });
@@ -100,6 +131,15 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
         Mnavi3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Mnavi1.setImageResource(R.drawable.ic_home);
+                Mnavi2.setImageResource(R.drawable.ic_hotshots__4_);
+                Mnavi3.setImageResource(R.drawable.ic_buzz_select);
+                Mnavi4.setImageResource(R.drawable.ic_headphones);
+
+//                naviText1.setTextColor(Color.parseColor("CACFD2"));
+//                naviText2.setTextColor(Color.parseColor("CACFD2"));
+//                naviText3.setTextColor(Color.parseColor("CACFD2"));
+//                naviText4.setTextColor(Color.parseColor("000000"));
                 AddBuzz();
             }
         });
@@ -114,9 +154,12 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
     }
 
     private void AddPodcast() {
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        PodcastFragment podcastFragment = new PodcastFragment();
-        fragmentTransaction.replace(R.id.fcContainer, podcastFragment, "Fragme3").commit();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        PodcastFragment podcastFragment = new PodcastFragment();
+//        fragmentTransaction.replace(R.id.fcContainer, podcastFragment, "Fragme3").commit();
+
+        Intent intent = new Intent(MainActivity.this,HotshotsActivity.class);
+        startActivity(intent);
 
 
     }
