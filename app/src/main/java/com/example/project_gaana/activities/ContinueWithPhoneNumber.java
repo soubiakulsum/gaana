aad9be67b0b3293b8a7f2290b301ea18ec0b5b4b
MINTU325 <sainimintu34@gmail.com>
2021-03-13 01:31:23 +0530

    EnterNumber

--- app/src/main/java/com/example/project_gaana/activities/ContinueWithPhoneNumber.java
+++ app/src/main/java/com/example/project_gaana/activities/ContinueWithPhoneNumber.java
@@ -2,15 +2,28 @@ package com.example.project_gaana.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.project_gaana.R;
 
 public class ContinueWithPhoneNumber extends AppCompatActivity {
+    private Button mBTNContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_continue_with_phone_number);
+        mBTNContinue= findViewById(R.id.btnHome45);
+        mBTNContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContinueWithPhoneNumber.this,EnterNumber.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/project_gaana/activities/SplashScreen.java
+++ app/src/main/java/com/example/project_gaana/activities/SplashScreen.java
@@ -31,7 +31,7 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        },2300);
 
     }
 }
