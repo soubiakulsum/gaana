6f5b6a9b82114884d55a9e9b487d7632e5291168
MINTU325 <sainimintu34@gmail.com>
2021-03-12 23:59:46 +0530

    splash screen

---
+++ app/src/main/java/com/example/project_gaana/activities/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.project_gaana.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.project_gaana.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        intiViews();
+    }
+
+    private void intiViews() {
+        timer =new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
